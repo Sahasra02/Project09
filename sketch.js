31bde2b2c31e7c3a590ae31d33bf9665ca7f8cf8
Sahasra02 <108621354+Sahasra02@users.noreply.github.com>
2022-07-10 10:53:16 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var ball
+function setup() {
+  createCanvas(400,400);
+ ball = createSprite(200,200,20,20)
+}
+
+function draw() 
+{
+ if (keyDown(UP_ARROW))
+
+  {
+  background("red")
+ }
+
+}
+ 
+
+
+
+
+
